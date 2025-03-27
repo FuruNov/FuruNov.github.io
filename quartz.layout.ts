d718c1b1f20e384674ec1e6bc9ef3069616d44fa
FuruNov <clz14116@nitech.jp>
2025-03-27 14:00:43 +0900

    Quartz sync: Mar 27, 2025, 2:00 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
     afterBody: [],
     footer: Component.Footer({
         links: {
-            GitHub: "https://github.com/jackyzha0/quartz",
-            "Discord Community": "https://discord.gg/cRFFHYye7t",
+            "Google Scholar": "https://scholar.google.com/citations?user=gvYEpF0AAAAJ",
+            "researchmap": "https://researchmap.jp/takanobu-furuhashi",
         },
     }),
 }
